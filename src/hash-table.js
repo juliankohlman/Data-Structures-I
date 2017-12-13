264067408352d0bea3bab40d301e953f01ad1185
Julian Kohlman <juliankohlman@gmail.com>
2017-12-12 19:22:44 -0500

    Test passing review Hash Tables more

--- src/hash-table.js
+++ src/hash-table.js
@@ -12,17 +12,45 @@ class HashTable {
   insert(key, value) {
     const index = getIndexBelowMax(key.toString(), this.limit);
     const bucket = this.storage.get(index);
+
+    if (bucket === undefined) {
+      this.storage.set(index, [[key, value]]);
+      return;
+    }
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        this.storage.set(index, bucket);
+        return;
+      }
+    }
+    // turns out that the key we're trying to insert is unique
+    // we can just add it to our bucket
+    bucket.push([key, value]);
+    this.storage.set(index, bucket);
   }
 
   remove(key) {
-    const removalBucket = getIndexBelowMax(key.toString(), this.limit);
-    return this.storage.set(removalBucket, undefined);
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const bucket = this.storage.get(index);
+    // return this.storage.set(removalBucket, undefined);
+    if (!bucket) return;
+    if (bucket.length === 1) return this.storage.set(index, undefined);
+    bucket.forEach((pair, i) => {
+      if (pair[0] === key) bucket.splice(i, 1);
+      this.storage.set(index, bucket);
+    });
   }
 
   retrieve(key) {
-    const valueBucket = getIndexBelowMax(key.toString(), this.limit);
-    if (this.storage.get(valueBucket) === undefined) return undefined;
-    return this.storage.get(valueBucket)[1];
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const bucket = this.storage.get(index);
+    // if (this.storage.get(index) === undefined) return undefined;
+    // return this.storage.get(index)[1];
+    if (!bucket) return;
+    for (let i = 0; i < bucket.length; i += 1) {
+      if (bucket[i][0] === key) return bucket[i][1];
+    }
   }
 }
 
